2ff8566b4ed4243610f45342f29cbf59335b3c8e
murat <killjoy_tr@yahoo.com>
2022-04-21 14:08:45 +0300

    Start to add Exchange model

--- backend/src/resolvers.ts
+++ backend/src/resolvers.ts
@@ -1,7 +1,83 @@
+import { GraphQLResolveInfo } from "graphql";
+import Post from "./models/Post.model";
+
+type CreatePostInputType = {
+  title: string;
+  description: string;
+};
+
+type CreatePostArgs = {
+  input: CreatePostInputType;
+};
+
+type UpdatePostInputType = {
+  title?: string;
+  description?: string;
+};
+
+type UpdatePostArgs = {
+  id: string;
+  input: UpdatePostInputType;
+};
+
+type DeletePostArgs = {
+  id: string;
+};
+
 const resolvers = {
   Query: {
     hello: () => {
       return "Hellooooo";
+    },
+
+    getAllPosts: async () => {
+      return await Post.find();
+    },
+
+    getPost: async (_: undefined, name: string) => {
+      return await Post.find();
+    },
+  },
+
+  Mutation: {
+    createPost: async (_: undefined, args: CreatePostArgs) => {
+      const {
+        input: { title, description },
+      } = args;
+
+      const post = new Post({
+        title,
+        description,
+      });
+
+      await post.save();
+
+      return post;
+    },
+
+    deletePost: async (_: undefined, args: DeletePostArgs) => {
+      const { id } = args;
+      await Post.findByIdAndDelete(id);
+      return id;
+    },
+
+    updatePost: async (_: undefined, { id, input }: UpdatePostArgs) => {
+      const { description, title } = input;
+
+      //const params = {};
+      // Object.entries(input)
+      //   .filter((f) => f[1])
+      //   .reduce((accum, [k, v]) => {
+      //     accum[k] = v;
+      //     return accum;
+      //   }, {});
+
+      const params = Object.fromEntries(
+        Object.entries(input).filter((f) => f[1])
+      );
+
+      await Post.findByIdAndUpdate(id, params);
+      return await Post.findById(id);
     },
   },
 };
--- backend/src/typeDefs.ts
+++ backend/src/typeDefs.ts
@@ -1,8 +1,33 @@
 import { gql } from "apollo-server-express";
 
 const typeDefs = gql`
+  type Post {
+    id: ID
+    title: String
+    description: String
+  }
+
   type Query {
     hello: String
+
+    getAllPosts: [Post]
+    getPost(name: String!): Post
+  }
+
+  input CreatePostInput {
+    title: String!
+    description: String
+  }
+
+  input UpdatePostInput {
+    title: String
+    description: String
+  }
+
+  type Mutation {
+    createPost(input: CreatePostInput!): Post
+    deletePost(id: ID!): String
+    updatePost(id: ID!, input: UpdatePostInput!): Post
   }
 `;
 
