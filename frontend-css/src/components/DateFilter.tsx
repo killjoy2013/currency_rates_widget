1cd9913c887e9adb910af32fb7f72a475036e1f7
murat <killjoy_tr@yahoo.com>
2022-04-22 18:57:17 +0300

    set query data works

--- frontend-css/src/components/DateFilter.tsx
+++ frontend-css/src/components/DateFilter.tsx
@@ -14,6 +14,7 @@ import graphqlRequestClient from "@src/lib/graphqlRequestClient";
 import { useQuery } from "react-query";
 import { GraphQLResponse } from "graphql-request/dist/types";
 import { useEffect } from "react";
+import { useQueryClient } from "react-query";
 
 type FormData = {
   fromDate: Date;
@@ -27,6 +28,8 @@ const DateFilter = () => {
     toDate: new Date(),
     type: PriceType.All,
   });
+
+  const queryClient = useQueryClient();
 
   const { data, error, isError, isLoading, refetch, isFetching } = useQuery<
     GetExchangesQuery,
@@ -117,6 +120,48 @@ const DateFilter = () => {
         onClick={async () => {
           let dell = await refetch();
           console.log("refetch", dell);
+        }}
+      />
+      <Button
+        label="setcachee"
+        variant="outlined"
+        onClick={async () => {
+          queryClient.setQueryData("exchange-rates", (old: any) => {
+            console.log({ old });
+
+            const { getExchanges } = old;
+
+            console.log({ getExchanges });
+
+            // const {
+            //   data: { getExchanges },
+            // } = old;
+
+            // console.log({ getExchanges });
+
+            // let newData: Array<Exchange> = [...data];
+
+            let newOne: Exchange = {
+              amount1: 99,
+              amount2: 88,
+              currencyFrom: "Bitcoin",
+              currencyTo: "USD",
+              dateTime: "2022-04-22 19:19",
+              type: PriceType.Exchanged,
+            };
+
+            return {
+              getExchanges: [newOne, ...getExchanges],
+            };
+          });
+
+          // queryClient.setQueryData("exchange-rates", (data) => {
+          //   console.log({ data });
+
+          //   return {
+          //     data: {},
+          //   };
+          // });
         }}
       />
     </div>
